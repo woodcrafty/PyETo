290bc57331d3bb305ed90402861f100f9ef32585
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-08 21:50:04 +0100

    Added test_suite, long_description etc.

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='eto',
-      version='0.1.0',
-      description="A library of functions for estimating potential evapotranspiration.",
-      author='Mark Richards',
-      author_email='mark.l.a.richards@abdn.ac.uk',
-      url='None',
-      packages=['eto']
-     )
-
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
+setup(
+    name='eto',
+    version='0.2.0',
+    description=(
+        'Library for estimating reference andnpotential evapotranspiration.'
+    ),
+    long_description=open('README.rst').read(),
+    author='Mark Richards',
+    author_email='mark.l.a.richardsREMOVETHIS@gmail.com',
+    license='BSD 3-Clause',
+    url='None',
+    packages=['pyeto'],
+    test_suite='tests',
+)
