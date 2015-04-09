c4e7f63cef1c91384c87f9e2c67f5843e389d301
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-09 11:15:23 +0100

    Added some comments

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -17,12 +17,14 @@ class TestConvert(unittest.TestCase):
 
     def test_degrees2radians(self):
         self.assertEqual(pyeto.degrees2radians(0), 0.0)
+        # Test values obtained form online conversion calculator
         self.assertAlmostEqual(pyeto.degrees2radians(-90), -1.5707963268, 10)
         self.assertAlmostEqual(pyeto.degrees2radians(90), 1.5707963268, 10)
         self.assertAlmostEqual(pyeto.degrees2radians(360), 6.2831853072, 10)
 
     def test_radians2degrees(self):
         self.assertEqual(pyeto.radians2degrees(0), 0.0)
+        # Test values obtained form online conversion calculator
         self.assertAlmostEqual(pyeto.radians2degrees(-1.5707963268), -90.0)
         self.assertAlmostEqual(pyeto.radians2degrees(1.5707963268), 90.0)
         self.assertAlmostEqual(pyeto.radians2degrees(6.2831853072), 360.0)
