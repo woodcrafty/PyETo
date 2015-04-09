14f80e52b0c4d172c2f17e22415871745672693a
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-09 13:21:47 +0100

    Added classifiers

--- setup.py
+++ setup.py
@@ -15,4 +15,18 @@ setup(
     packages=['pyeto'],
     package_data={'': ['*.rst', '*.txt']},
     test_suite='tests',
+    classifiers=[
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
 )
