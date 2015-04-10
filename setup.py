a83d89e8f6416689f258f5ec92ce29e38243913f
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-10 17:59:00 +0100

    Removed Python 2.6 support (due to unittest assertAlmostEqual nto supporting the delta argument in 2.6)

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     test_suite='tests',
     classifiers=[
         'Programming Language :: Python',
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
