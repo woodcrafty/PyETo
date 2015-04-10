63d2c080c0d33ad13ffe3fdb700f262d97c44c05
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-10 16:35:57 +0100

    Added test_thornthwaite()

--- tests/test_thornthwaite.py
+++ tests/test_thornthwaite.py
@@ -10,6 +10,56 @@ import pyeto
 class TestThornthwaite(unittest.TestCase):
 
     def test_monthly_mean_daylight_hours(self):
+        # Test against values for latitude 20 deg N from Bautista et al (2009)
+        # Calibration of the equations of Hargreaves and Thornthwaite to
+        # estimate the potential evapotranspiration in semi-arid and subhumid
+        # tropical climates for regional applications. Atmosfera 22(4), 331-
+        # 348.
+        test_mmdlh = [
+            10.9,  # Jan
+            11.3,  # Feb
+            11.9,  # Mar
+            12.5,  # Apr
+            12.9,  # May
+            13.2,  # Jun
+            13.1,  # Jul
+            12.7,  # Aug
+            12.1,  # Sep
+            11.5,  # Oct
+            11.0,  # Nov
+            10.8,  # Dec
+        ]
+        mmdlh = pyeto.monthly_mean_daylight_hours(pyeto.degrees2radians(20.0))
+        # Values were only quoted to 1 decimal place so check they are accurate
+        # to within 12 minutes (0.2 hours)
+        for m in range(12):
+            self.assertAlmostEqual(mmdlh[m], test_mmdlh[m], delta=0.15)
+
+        # Test against values for latitude 46 deg N from Mimikou M. and
+        # Baltas E., Technical hydrology, Second edition, NTUA, 2002.
+        # cited in PAPADOPOULOU E., VARANOU E., BALTAS E., DASSAKLIS A., and
+        # MIMIKOU M. (2003) ESTIMATING POTENTIAL EVAPOTRANSPIRATION AND ITS
+        # SPATIAL DISTRIBUTION IN GREECE USING EMPIRICAL METHODS.
+        test_mmdlh = [
+            8.9,   # Jan
+            10.1,  # Feb
+            11.6,  # Mar
+            13.3,  # Apr
+            14.7,  # May
+            15.5,  # Jun
+            15.2,  # Jul
+            13.9,  # Aug
+            12.3,  # Sep
+            10.7,  # Oct
+            9.2,   # Nov
+            8.5,   # Dec
+        ]
+        mmdlh = pyeto.monthly_mean_daylight_hours(pyeto.degrees2radians(46.0))
+        # Values were only quoted to 1 decimal place so check they are accurate
+        # to within 12 minutes (0.2 hours)
+        for m in range(12):
+            self.assertAlmostEqual(mmdlh[m], test_mmdlh[m], delta=0.15)
+
         # Test against values obtained for Los Angles, California,
         # latitude 34 deg 05' N, from
         # http://aa.usno.navy.mil/data/docs/Dur_OneYear.php
@@ -50,6 +100,25 @@ class TestThornthwaite(unittest.TestCase):
 
         _ = pyeto.monthly_mean_daylight_hours(
             pyeto.degrees2radians(-90.0))
+
+    def test_thornthwaite(self):
+        # Test values obtained from a worked example provided in Google book
+        test_monthly_t = [
+            2.1, 2.5, 4.8, 7.1, 8.3, 10.7, 13.4, 14.5, 11.1, 8.2, 5.4, 3.7]
+        test_monthly_mean_dlh = [
+            9.4, 10.6, 11.9, 13.4, 14.6, 15.2, 14.9, 13.9, 12.6, 11.1, 9.8, 9.1]
+        test_pet = [
+            10.67, 14.08, 28.49, 45.85, 57.47, 75.20, 89.91, 90.29, 64.26,
+            43.34, 26.24, 17.31]
+
+        # NOTE: The test PET was calculated using rounded coefficients, rounded
+        # intermediate values and doesn't adjust for the number of days in
+        # the month. This results in a small difference in estimated monthly
+        # PET of up to +/- 4 mm.
+        pet = pyeto.thornthwaite(test_monthly_t, test_monthly_mean_dlh)
+        for i in range(len(pet)):
+            diff = abs(pet[i] - test_pet[i])
+            self.assertLess(diff, 4)
 
 
 if __name__ == '__main__':
