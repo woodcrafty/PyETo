73bade84556c5a60b3b3964938d62a185937ebfa
woodcrafty <mark.l.a.richards@gmail.com>
2015-04-13 14:58:16 +0100

    Added test for check_sunset_hour_angle()

--- tests/test_check.py
+++ tests/test_check.py
@@ -10,6 +10,16 @@ from pyeto.convert import deg2rad
 
 class TestCheck(unittest.TestCase):
 
+    def test_check_day_hours(self):
+        with self.assertRaises(ValueError):
+            _check.check_day_hours(-1, 'test')
+
+        with self.assertRaises(ValueError):
+            _check.check_day_hours(25, 'test')
+
+        self.assertIsNone(_check.check_day_hours(0, 'test'))
+        self.assertIsNone(_check.check_day_hours(24, 'test'))
+
     def test_check_doy(self):
         with self.assertRaises(ValueError):
             _check.check_doy(0)
@@ -22,30 +32,30 @@ class TestCheck(unittest.TestCase):
 
     def test_check_latitude_rad(self):
         with self.assertRaises(ValueError):
-            _check.check_latitude_rad(deg2rad(-91))
+            _check.check_latitude_rad(deg2rad(-90.0001))
 
         with self.assertRaises(ValueError):
-            _check.check_latitude_rad(deg2rad(91))
+            _check.check_latitude_rad(deg2rad(90.0001))
 
         self.assertIsNone(_check.check_latitude_rad(deg2rad(-90)))
         self.assertIsNone(_check.check_latitude_rad(deg2rad(90)))
 
     def test_check_sol_dec_rad(self):
         with self.assertRaises(ValueError):
-            _check.check_sol_dec_rad(deg2rad(-91))
+            _check.check_sol_dec_rad(deg2rad(-23.5001))
 
         with self.assertRaises(ValueError):
-            _check.check_sol_dec_rad(deg2rad(91))
+            _check.check_sol_dec_rad(deg2rad(23.5001))
 
-        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(-90)))
-        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(90)))
+        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(-23.5)))
+        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(23.5)))
 
-    def test_check_day_hours(self):
+    def test_check_sunset_hour_angle(self):
         with self.assertRaises(ValueError):
-            _check.check_day_hours(-1, 'test')
+            _check.check_sunset_hour_angle(deg2rad(-0.00001))
 
         with self.assertRaises(ValueError):
-            _check.check_day_hours(25, 'test')
+            _check.check_sunset_hour_angle(deg2rad(180.00001))
 
-        self.assertIsNone(_check.check_day_hours(0, 'test'))
-        self.assertIsNone(_check.check_day_hours(24, 'test'))
+        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(0.0)))
+        self.assertIsNone(_check.check_sol_dec_rad(deg2rad(180.0)))
